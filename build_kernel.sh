3137b6538dd993e2f67649a3602d70e1cc9b43df
poss03251 <mpossamaim@gmail.com>
2024-12-05 20:44:28 +0000

    Relying on external code to start the Kernel. Try at the moment to go down this path and come back later

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,27 +3,9 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Set script variables
-PROJECT_DIR="$(pwd)"
-KERNEL_NAME="spaghettos_kernel_rs"
-BUILD_DIR="${PROJECT_DIR}/build"
+OS_FILE=target/x86_64_spaghettos/debug/bootimage-spaghettos_kernel_rs.bin
 
-# Navigate to the kernel project directory
-cd "$PROJECT_DIR"
+# Build with cargo and `bootimage` library
+cargo bootimage --target x86_64_spaghettos.json
 
-# Ensure the build directory exists
-mkdir -p "$BUILD_DIR"
-
-cargo build --release --target x86_64-unknown-none -q 
-
-# Path to the compiled kernel
-KERNEL_LIB="${PROJECT_DIR}/target/x86_64-unknown-none/release/lib${KERNEL_NAME}.a"
-
-# Check if the compiled kernel exists
-if [ ! -f "$KERNEL_LIB" ]; then
-    echo "Error: Compiled kernel not found at $KERNEL_LIB"
-    exit 1
-fi
-
-# Copy the kernel binary to the build directory
-cp "$KERNEL_LIB" "$BUILD_DIR/libkernel.a"
+qemu-system-x86_64 -display sdl -drive format=raw,file="$OS_FILE" -d cpu_reset -d int -no-reboot -no-shutdown
---
+++ build_kernel_legacy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Set script variables
+PROJECT_DIR="$(pwd)"
+KERNEL_NAME="spaghettos_kernel_rs"
+BUILD_DIR="${PROJECT_DIR}/build"
+
+# Navigate to the kernel project directory
+cd "$PROJECT_DIR"
+
+# Ensure the build directory exists
+mkdir -p "$BUILD_DIR"
+
+cargo build --release --target x86_64-unknown-none -q 
+
+# Path to the compiled kernel
+KERNEL_LIB="${PROJECT_DIR}/target/x86_64-unknown-none/release/${KERNEL_NAME}"
+
+# Check if the compiled kernel exists
+if [ ! -f "$KERNEL_LIB" ]; then
+    echo "Error: Compiled kernel not found at $KERNEL_LIB"
+    exit 1
+fi
+
+# Copy the kernel binary to the build directory
+# cp "$KERNEL_LIB" "$BUILD_DIR/kernel"
+objcopy -O binary "$KERNEL_LIB" "$BUILD_DIR/kernel.bin"
